3d6522f4d10613d40b916bedcf68b76f60c86fba
tommyloh <tommyloh92@gmail.com>
2016-07-22 17:05:04 +0800

    picture can be uploaded to firebase, pickerView is working, but cant upload to firebase yet.

--- Exire/CreateViewController.swift
+++ Exire/CreateViewController.swift
@@ -9,35 +9,81 @@
 import UIKit
 import Firebase
 
-class CreateViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate{
-    @IBOutlet var imageView: UIImageView!
-    
 
+class CreateViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDelegate , UIPickerViewDataSource {
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet var imageView: UIImageView!
+    var pickerData: [String] = [String]()
+    let firebaseDatabaseRef = FIRDatabase.database().reference()
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
-        
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
+        pickerData = ["Music", "Sports", "Night Life", "Learning", "Expos", "Networking"]
     }
-
+    
     @IBAction func onSelectButtonPressed(sender: AnyObject) {
-        imagePicker.allowsEditing = false
+        imagePicker.allowsEditing = true
         imagePicker.sourceType = .PhotoLibrary
         
         presentViewController(imagePicker, animated: true, completion: nil)
     }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            imageView.contentMode = .ScaleAspectFit
-            imageView.image = pickedImage
+        var pickedImage = UIImage?()
+        if let EdittedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            pickedImage = EdittedImage
+        }else if let OriginalImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            pickedImage = OriginalImage
         }
-        
+            imageView.contentMode = .ScaleToFill
+            imageView.image = pickedImage
         dismissViewControllerAnimated(true, completion: nil)
-    }
-    
+        }
+   
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+   
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerDidSelect(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(row)
+        
+    }
+    
+    
+    @IBAction func onPostEventPressed(sender: UIButton) {
+        
+        let randomUID = NSUUID().UUIDString
+        let firebaseStorageRef = FIRStorage.storage().reference().child("EventPicture").child("\(randomUID).jpeg")
+        
+        if let selectedImageData = UIImageJPEGRepresentation(self.imageView.image!, 0.1){
+            let metadata = FIRStorageMetadata()
+            metadata.contentType = "image/jpeg"
+            firebaseStorageRef.putData(selectedImageData, metadata: metadata, completion: { (metadata, error ) in
+                if error != nil{
+                    print(error)
+                    
+                    return
+                }else if let eventImageUrl = metadata?.downloadURL()?.absoluteString {
+                    let values = ["EventpictureURL" : eventImageUrl]
+                    self.firebaseDatabaseRef.child("users").child(User.currentUserUid()!).child("Customer_picture").setValue(values)
+                }
+            })
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
+
--- Podfile
+++ Podfile
@@ -3,11 +3,13 @@
 # Comment this line if you're not using Swift and don't want to use dynamic frameworks
 use_frameworks!
 target 'Exire' do
-pod 'Firebase'
-pod 'Firebase/Database'
+    pod 'Firebase'
+    pod 'Firebase/Auth'
+    pod 'Firebase/Database'
 pod 'Alamofire', '~> 3.4'
 pod 'SwiftyJSON'
 pod 'Firebase/Auth'
+pod 'Firebase/Storage'
 # Pods for Exire
 
 end
